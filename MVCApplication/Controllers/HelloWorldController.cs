acf147de4589b2b1e4afde7ea66989c47b8cc519
pakosam <pako.s@yahoo.com>
2024-11-09 15:22:16 +0100

    Second part - final changes

--- MVCApplication/Controllers/HelloWorldController.cs
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using System.Linq.Expressions;
+using System.Xml;
 
 namespace MVCApplication.Controllers
 {
@@ -6,11 +9,27 @@ namespace MVCApplication.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(dogs);
         }
         public string Hello()
         {
             return "Hello";
+        }
+        private static List<Dog> dogs = new List<Dog>()
+        {
+            new Dog {Name = "Rex", Age = 3 },
+            new Dog {Name = "Spike", Age = 1 },
+            new Dog {Name = "Buddy", Age = 5 }
+        };
+        public IActionResult Create()
+        {
+            Dog dog = new Dog();
+            return View(dog);
+        }
+        public IActionResult CreateDog(Dog dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction("Index");
         }
     }
 }
