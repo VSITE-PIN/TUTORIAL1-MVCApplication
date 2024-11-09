94732cf9f3864632afa14cf8cadc85f9468d2685
Ivan Ceranac <ivcerana@vsite.hr>
2024-11-09 15:31:23 +0100

    Riješena vježba Ivan Ceranac (2012115)

---
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		public IActionResult Index()
+		{
+			//Dog dog = new Dog();
+			//dog.Name = "Rex";
+			//dog.Age = 2;
+			return View(dogs);
+		}
+
+		public string Hello()
+		{
+			return "Hello";
+		}
+
+		private static List<Dog> dogs = new List<Dog>();
+
+		public IActionResult Create()
+		{
+			Dog dog = new Dog();
+			return View(dog);
+		}
+
+		public IActionResult CreateDog(Dog dogViewModel)
+		{
+			dogs.Add(dogViewModel);
+			return RedirectToAction("Index");
+		}
+
+
+	}
+}
