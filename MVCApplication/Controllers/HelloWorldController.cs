7ac84022ba1e1cb4e14586d82c37bf6466548909
breski-igor <igor.breski@vsite.hr>
2024-11-10 10:03:32 +0100

    igor_breski_v02

---
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        
+      
+        public string Hello()
+        {
+            return "hello";
+        }
+
+        private static List<Dog> dogs = new List<Dog>()
+        {
+            new Dog { Name = "Rex", Age = 3}
+        };
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            Dog dog = new Dog();
+            return View(dog);   
+        }
+
+        public IActionResult CreateDog(Dog dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCApplication/Models/Dog.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCApplication.Models
+{
+    public class Dog
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
