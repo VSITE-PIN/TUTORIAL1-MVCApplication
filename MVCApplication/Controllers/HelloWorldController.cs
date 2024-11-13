6d71fe1078fecf3141622c189d98043e46466b4a
Davor Petrlić <dapetrli@vsite.hr>
2024-11-13 15:01:32 +0100

    Resolved first part of tutorial

---
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public String Hello()
+        {
+            return "Hello";
+        }
+    }
+}
