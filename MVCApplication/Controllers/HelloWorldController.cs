b1ce76f44b42c3c822ccba688a90b773c85e4a1f
Mislav Ćavarušić <micavaru@vsite.hr>
2024-12-10 19:33:54 +0100

    Resolved FirstMVCApp Tutorijal

---
+++ MVCApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<Dog> dogs = new List<Dog>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            Dog dog = new Dog();
+            return View(dog);
+        }
+        public string Hello()
+        {
+            return "Hello";
+        }
+        public IActionResult CreateDog(Dog dogViewModel)
+        { 
+            dogs.Add(dogViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
